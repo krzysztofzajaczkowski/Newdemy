b3dfae25c4c41832a70511cc6b98630b4c99e4e9
Olczix <61705277+Olczix@users.noreply.github.com>
2020-10-22 18:44:50 +0200

    Update course.py

--- utils/WebCrawler/course.py
+++ utils/WebCrawler/course.py
@@ -1,10 +1,13 @@
 import json
+import sys
+
 
 class Course:
-    def __init__(self, title, description, author, rating, price, imageSource, length, level):
+    def __init__(self, title, description, author, rating, price, imageSource, length, level, URL):
         self.imageSource = imageSource
         self.title = title
         self.desciption = description
+        self.category = None
         self.author = author
         self.rating = rating
         self.price = 'Darmowy'if price == 'Free' else price[2:] + ' zł'
@@ -16,16 +19,27 @@ class Course:
                 else (length[0:length.find(' ')] + ' godzina' if length[len(length)-4:len(length)] == 'hour' \
                 else (length[0:length.find(' ')] + ' minut' if length[len(length)-4:len(length)] == 'mins' \
                 else length))
- 
+        self.URL = URL
+        self.extendedDescription = None
+
+    def setCategory(self, category):
+        self.category = category
+
+    def setExtendedDescription(self, extendedDescription):
+        self.extendedDescription = extendedDescription
+
     def makeJSON(self):
         dataString = {}
         dataString['title'] = self.title
         dataString['description'] = self.desciption
         dataString['author'] = self.author
+        dataString['category'] = self.category
         dataString['rating'] = self.rating
         dataString['price'] = self.price
         dataString['imageSource'] = self.imageSource
         dataString['length'] = self.length
         dataString['level'] = self.level
-   
+        dataString['URL'] = self.URL
+        dataString['extendedDescription'] = self.extendedDescription
+
         return dataString
