daf8df4a5d8e050d4dc60b37fb73bc9488d2e040
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-10-27 15:43:35 +0100

    ND-8: Add initial import solution

---
+++ utils/ImportProducts/PrestaShopAPI/PrestaShopAPI/Initializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace PrestaShopAPI
+{
+    public class Initializer
+    {
+        private readonly HttpClient _imgHttpClient;
+        private readonly IConfiguration _config;
+        private readonly ILogger<Initializer> _logger;
+
+        public Initializer(IHttpClientFactory httpClientFactory, IConfiguration configuration, ILogger<Initializer> logger)
+        {
+            _imgHttpClient = httpClientFactory.CreateClient();
+            _config = configuration;
+            _logger = logger;
+        }
+
+        public async Task InitializeProducts()
+        {
+
+        }
+    }
+}
